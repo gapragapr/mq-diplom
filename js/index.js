6b4a6e8d3c5fb17ad7aa01f50e48d799ea91fda0
gapragapr <78931859+gapragapr@users.noreply.github.com>
2021-07-12 13:54:17 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openFeedback(open = true) {
+    const feedbackpopup = document.getElementById('feedback-popup');
+    feedbackpopup.style.display = open ? 'flex' : 'none';
+}
+
+function openHamburger(open = true) {
+    const hamburgermenu = document.getElementById('hamburger');
+    hamburgermenu.style.right = open ? '0' : '-100%'  
+}
